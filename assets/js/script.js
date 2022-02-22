d4d4b48352ee209d1b42fc25f4f9b7c68c6b55d8
Gabriel Cantesani <cantesani@hotmail.com>
2022-02-21 22:11:52 -0300

    inicio de configuração de mascaras

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,39 +23,52 @@ if (loja.length === 0) {
     document.getElementById("nenhuma-transacao").style.display = "none";
 }
 
-for (item in loja) {
-    document.querySelector('table.tabela-extrato tbody').innerHTML +=
-        `<tr>
+var numeroPadrao = /[^0-9.,]/
 
-            <td class="extrato-btn"> 
-                ${loja[item].tipoTransacao}
-            </td>
 
-            <td class="extrato-mercadoria">
-                ${loja[item].mercadoria}
+function testaCampoValor(e) {
+    e.preventDefault();
 
-            </td>
+    if (numeroPadrao.test(e.key)) {
+        console.log(e.key);
+        e.preventDefault();
+        return;
+    }
 
-            <td class="extrato-valor"> 
-                ${loja[item].valor}
-            </td>
-        </tr>
-        `
+    valor = e.target.value.toString();
+    valor = valor.replace(/[\D]+/g, "");
+    valor = valor.replace(/([0-9]{1})$/g, ",$1");
+
+    if (valor.length >= 6) {
+        while (/([0-9]{4})[,|\.]/g.test(valor)) {
+            valor = valor.replace(/([0-9]{1})$/g, ",$1");
+            valor = valor.replace(/([0-9]{3})[,|\.]/g, ".$1");
+        }
+    }
+    e.target.value = valor;
+
+    if ((/[0-9,.]+/g).test(e.key)) {
+        e.target.value += e.key;
+    }
 }
 
 function testaFormulario(e) {
     e.preventDefault();
 
-    var numeroPattern = /[^0-9R$,. ]+/g
-
-    if (numeroPattern.test(e.target.elements['valor'].value)) {
+    if (numeroPadrao.test(e.target.elements['valor'].value)) {
         alert('Apenas números são permitidos no campo VALOR')
         return false
     }
 
-    if (e.target.elements['valor'].value.replace(/[R$,. ]/g, '').length < 4) {
-        alert('O campo VALOR precisa ser preenchido CORRETAMENTE! \n Exemplo: 10,90')
-        return false;
+    // if ((/[0-9,.]+/g).test(e.key)) {
+    //     e.target.value += e.key;
+    // }
+
+    // -------------------------------------
+    // PAREI AQUI
+    // ------------------------------------
+    if (e.target.elements['valor'].value.replace(/[,.]+/g, "")) {
+        console.log('oi')
     }
 
     if (e.target.elements['mercadoria'].value == '') {
@@ -76,11 +89,6 @@ function testaFormulario(e) {
         var loja = [];
     }
 
-    if (lojaCru == []) {
-        console.log('vazio')
-    }
-
-
 
     var alteraTransacao = e.target.elements['transacao'].value
 
@@ -90,32 +98,42 @@ function testaFormulario(e) {
         var alteraTransacao = '-'
     }
 
+    var valorCRU = e.target.elements['valor'].value
+    var valorBRL = (new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(valorCRU));
+
+    // console.log(valorCRU)
+    // console.log(valorBRL)
+    // e.preventDefault()
+
     loja.push({
         tipoTransacao: alteraTransacao,
         mercadoria: e.target.elements['mercadoria'].value,
-        valor: e.target.elements['valor'].value,
+        valor: valorBRL
+        // .replaceAll(".", "")
+        // .replaceAll(",", "."),
     })
 
     localStorage.setItem('loja', JSON.stringify(loja))
     window.location.href = "./index.html"
 }
 
-function testaCampoValor(e) {
-    e.preventDefault();
+for (item in loja) {
+    document.querySelector('table.tabela-extrato tbody').innerHTML +=
+        `<tr>
 
-    if (e.target.value.length == 0) {
-        e.target.value += 'R$ '
-    }
+            <td class="extrato-btn"> 
+                ${loja[item].tipoTransacao}
+            </td>
 
-    if (e.target.value.length == 5) {
-        e.target.value += ','
-    }
+            <td class="extrato-mercadoria">
+                ${loja[item].mercadoria}
 
-    if ((/[0-9]+/g).test(e.key) && e.target.value.length < 8) {
-        e.target.value += e.key;
-    }
+            </td>
+
+            <td class="extrato-valor"> 
+                ${loja[item].valor}
+            </td>
+        </tr>
+        `
 }
-
-
-// JS DO FORMULÁRIO
 
